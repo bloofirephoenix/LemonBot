8a821c10f8b60408196a601cfc712dec4627cc8c
Bloo <17679976+bloofirephoenix@users.noreply.github.com>
2024-01-25 21:12:55 -0800

    please work lemonbot

--- LemonBot/Features/Memes/MemeManager.cs
+++ LemonBot/Features/Memes/MemeManager.cs
@@ -2,16 +2,17 @@ using System.Runtime.InteropServices;
 using Discord.WebSocket;
 using LemonBot.Configurations;
 using LemonBot.Utilities;
+using static LemonBot.Configurations.DailyMemes;
 
 namespace LemonBot.Features.Memes;
 
 public class MemeManager
 {
-    public static bool Started { get; private set; }
+    private Task? _task;
     private DiscordSocketClient _client;
     private DailyMemes _memes;
     private MemeCommand _command;
-    
+
     public MemeManager(DiscordSocketClient client)
     {
         _client = client;
@@ -22,15 +23,13 @@ public class MemeManager
 
     public void Start()
     {
-        if (Started)
+        if (_task != null && !_task.IsCompleted)
         {
             Logger.Warning("Tried to start the MemeManager but it is already running");
             return;
         }
-        
-        Started = true;
-        
-        Task.Run(async () =>
+
+        _task = Task.Run(async () =>
         {
             Console.WriteLine("Starting daily memes");
             await _command.Register();
@@ -49,34 +48,77 @@ public class MemeManager
                 var dif = nextRun - DateTime.Now;
                 Console.WriteLine($"Scheduling the next meme for {nextRun.ToString(TimeFormat.Format)} in {dif}");
                 await Task.Delay(dif);
+                Console.WriteLine("It's meme time");
 
                 var dm = await _client.GetUser(259483144934260755).CreateDMChannelAsync();
+
+                if (dm == null) 
+                    Logger.Error("DM channel is null");
+
                 // send the meme
                 if (_memes.Memes.Count > 0)
                 {
                     var meme = _memes.Memes[0];
                     _memes.Memes.Remove(meme);
-                    await _client.GetGuild(_memes.Guild).GetTextChannel(_memes.TextChannel).SendFileAsync(meme.File,
-                        text: $"{_memes.UniversalMessage.Replace("%day%", _memes.Day.ToString())} {meme.Message}");
 
-                    // move the meme to posted folder
-                    try
+                    int tries = 0;
+                    while (tries < 60)
                     {
-                        Directory.CreateDirectory(_memes.PostedLocation);
-                        File.Move(meme.File, $"{_memes.PostedLocation}/{_memes.Day}{Path.GetExtension(meme.File)}");
+                        try
+                        {
+                            Console.WriteLine("Sending the meme");
+                            await _client.GetGuild(_memes.Guild).GetTextChannel(_memes.TextChannel).SendFileAsync(
+                                meme.File,
+                                text: $"{_memes.UniversalMessage.Replace("%day%", _memes.Day.ToString())} {meme.Message}");
+
+                            if (dm != null) 
+                                await dm.SendMessageAsync($"``{_memes.Memes.Count}`` memes left in the queue.");
+
+                            Console.WriteLine("Meme sent!");
+
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.Error(e.ToString());
+                            tries++;
+                            await Task.Delay(1000 * 60);
+                        }
                     }
-                    catch (Exception ex)
+
+                    if (tries < 60)
                     {
-                        Logger.Error(ex.ToString());
-                        await dm.SendMessageAsync($"``An error occurred moving the meme #{_memes.Day} ({meme.File})``");
+                        // move the meme to posted folder
+                        try
+                        {
+                            Console.WriteLine("Moving meme");
+                            Directory.CreateDirectory(_memes.PostedLocation);
+                            File.Move(meme.File, $"{_memes.PostedLocation}/{_memes.Day}{Path.GetExtension(meme.File)}");
+                            Console.WriteLine("Meme moved!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Error(ex.ToString());
+                            if (dm != null)
+                                await dm.SendMessageAsync(
+                                    $"``An error occurred moving the meme #{_memes.Day} ({meme.File})``");
+                        }
                     }
                 }
-                
-                // send the number of memes left to me in a dm
-                await dm.SendMessageAsync($"``{_memes.Memes.Count}`` memes left in the queue.");
 
-                _memes.Day++;
-                ConfigFile.Save(_memes);
+                try
+                {
+                    Console.WriteLine("Incrementing Day");
+                    _memes.Day++;
+                    ConfigFile.Save(_memes);
+                    Console.WriteLine("Day incremented!");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex.ToString());
+                    if (dm != null)
+                        await dm.SendMessageAsync($"``Error incrementing day``");
+                }
             }
         });
     }
