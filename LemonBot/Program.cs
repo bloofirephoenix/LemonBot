124a7719ab332de54794286527e062da2db9955b
Bloo <17679976+bloofirephoenix@users.noreply.github.com>
2024-02-05 08:32:35 -0800

    frick

--- LemonBot/Program.cs
+++ LemonBot/Program.cs
@@ -5,7 +5,6 @@ using LemonBot.Configurations;
 using LemonBot.Features;
 using LemonBot.Features.Memes;
 using LemonBot.Utilities;
-using System;
 
 _ = new Logger();
 
@@ -16,13 +15,13 @@ ConfigFile.Load(config);
 
 if (!ConfigFile.Load(config))
 {
-    Console.WriteLine("Please setup config.json");
-    return;
+	Console.WriteLine("Please setup config.json");
+	return;
 }
 
 var client = new DiscordSocketClient(new DiscordSocketConfig()
 {
-    GatewayIntents = GatewayIntents.GuildMessages | GatewayIntents.Guilds
+	GatewayIntents = GatewayIntents.GuildMessages | GatewayIntents.Guilds
 });
 
 client.Log += Logger.DiscordLog;
@@ -30,11 +29,14 @@ client.Log += Logger.DiscordLog;
 await client.LoginAsync(TokenType.Bot, config.Discord);
 await client.StartAsync();
 
+var memeManager = new MemeManager(client);
+var sendMessage = new SendMessage(client);
+
 client.Ready += async Task () =>
 {
-    Console.WriteLine("Bot Ready");
-    new MemeManager(client).Start();
-    await new SendMessage(client).Start();
+	Console.WriteLine("Bot Ready");
+	memeManager.Start();
+	await sendMessage.Start();
 };
 
 TerminalManager.Start();
